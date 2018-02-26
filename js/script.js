b141860b3706616bdba78a52e9a99acf2d98bf25
Bryan Jones <thebeard@me.com>
2018-02-25 20:01:19 -0800

    Cleaned up - Main page complete

--- js/script.js
+++ js/script.js
@@ -13,13 +13,21 @@
 
 
 
-
+/* === Carousel Speed === */
 $('.carousel').carousel({
     interval: 3000
-  });
+});
+/* ===^^^ Carousel Speed ^^^=== */
 
 
+/* ==== Modile Button ==== */
+$( " button.navbar-toggler" ).click(function() {
+    $('#mainImg, #myCarousel').toggleClass('blur'); 
+});
+/* ====^^^ Modile Button ^^^==== */
 
+
+/* === Mattress Modal === */
 $('#mattressModal').on('show.bs.modal', function (event) {
 
     var button = $(event.relatedTarget) // Button that triggered the modal
@@ -34,21 +42,24 @@ $('#mattressModal').on('show.bs.modal', function (event) {
         name: 'Belleza Classica',
         comfortLevel: 'Medium Firm',
         topperType: 'Pillow Top',
-        info: '<li>Memory Foam Infused with Gel Pillow Top</li> <li>Individual Pocket Coil Unit</li><li>Foam Encasement</li><li>Blue Suede Border</li>',
-        price: '<li>Full: $1399</li> <li>Queen: $1999</li> <li>California King: $2399</li> <li>Easter King: $2399</li> '
+        info: '<ul><li>Memory Foam Infused with Gel Pillow Top</li> <li>Individual Pocket Coil Unit</li></ul>  <ul><li>Foam Encasement</li><li>Blue Suede Border</li></ul>',
+        price: '<li>Full: $1399</li> <li>Queen: $1999</li> <li>California King: $2399</li> <li>Easter King: $2399</li> ',
+        img: './assets/Belleza-Chart.png'
+        
     };
 
     var chiro = {
-        name: 'chiro',
-        comfortLevel: '2Medium Firm',
-        topperType: '2Pillow Top',
-        info: '<li>2Memory Foam Infused with Gel Pillow Top</li> <li>2Individual Pocket Coil Unit</li> <li>2Foam Encasement</li> <li>2Blue Suede Border</li>',
-        price:{ twin: '2$1399',
-                full: '2$1700',
-                queen: '2$1999',
-                calKing: '2$2399',
-                eastKing: '2$2399'
-                }
+        name: 'Chiro Care',
+        comfortLevel: 'Medium Firm',
+        topperType: 'Pillow Top',
+        info: '<ul><li>Memory Foam Infused with Gel Pillow Top</li> <li>Individual Pocket Coil Unit</li></ul> <ul><li>Foam Encasement</li> <li>Blue Suede Border</li></ul>',
+        price:{ twin: '$1399',
+                full: '$1700',
+                queen: '$1999',
+                calKing: '$2399',
+                eastKing: '$2399'
+                },
+        img: './assets/Belleza-Chart.png'
     };
 
 
@@ -56,30 +67,32 @@ $('#mattressModal').on('show.bs.modal', function (event) {
                 Belleza, chiro
     ];
 
-    console.log(recipient);
+    // console.log(recipient);
     
 
     for(var i=0; i < mattress.length;i++){
         if(mattress[i].name == recipient){
-            console.log(mattress[i].name, 'here ' + i);
+            // console.log(mattress[i].name, 'here ' + i);
             var info = mattress[i].info;
             var price = mattress[i].price;
             var comfortLevel = mattress[i].comfortLevel;
             var topperType = mattress[i].topperType;
+            var img = $( '<img />' );
+            img.attr( 'src', mattress[i].img );
             
         }
     }
     
-
-    
-
-
     var modal = $(this)
     
     modal.find('.modal-title').html( recipient )
-    modal.find('.modal-body .modalLeft').html( info )
-    modal.find('.modal-body .modalRight').html( price )
-    modal.find('.modal-body .comfLevel').html( comfortLevel )
+    // modal.find('.modal-body .modalLeft').html( info )
+    modal.find('.modal-body .modalTop').html( info  )
+    // modal.find('.modal-body .modalRight').html( price )
+
+    modal.find('.modal-body .modalBottom').html(img )
+
+    // modal.find('.modal-body .comfLevel').html( comfortLevel )
     modal.find('.modal-body .topperType').html( topperType )
     
     
@@ -87,4 +100,6 @@ $('#mattressModal').on('show.bs.modal', function (event) {
     // modal.find('.modal-body input').val(recipient)
 
 
-  })
+  });
+/* ===^^^ Mattress Modal ^^^=== */
+
